d6338fc066a649506587b54f4c4d47e26433d770
Tysum <vinamraraaz@gmail.com>
2025-03-11 15:24:04 +0530

    Created a Patient entity model and made changes in Application properties file for H2 in-memory database

---
+++ patient-service/src/main/java/org/example/patientservice/model/Patient.java
@@ -0,0 +1,38 @@
+package org.example.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String dateOfBirth;
+
+    @NotNull
+    private String gender;
+
+    @NotNull
+    private LocalDate registrationDate;
+}
